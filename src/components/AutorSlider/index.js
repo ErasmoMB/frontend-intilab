8c3aedb5082ff1836f1c59fde0d87dd4df67db72
ErasmoMB <montufarbe@gmail.com>
2024-07-15 19:16:16 -0500

    subido al servidor lunes

--- src/components/AutorSlider/index.js
+++ src/components/AutorSlider/index.js
@@ -1,29 +1,32 @@
 import React, { useState, useEffect } from "react";
+import { useSwipeable } from "react-swipeable";
 import {
   obtenerDatosBasicosAutores,
   obtenerAutores,
   obtenerDocumentos,
 } from "../Api";
-import "./styles.css"; // Importar estilos CSS
+import "./styles.css"; // Estilos CSS personalizados
+import uch from "../../assets/Logos/logo-uch.png";
+import intilab from "../../assets/Logos/intilab.png";
+import ehealth from "../../assets/Logos/ehealth.png";
+import ciics from "../../assets/Logos/ciics.png";
 
 const AutorSlider = () => {
   const [autoresData, setAutoresData] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [loading, setLoading] = useState(true);
-  const [autoresLoadedCount, setAutoresLoadedCount] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [slideDirection, setSlideDirection] = useState("left");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Obtener datos básicos de autores
         const datosResponse = await obtenerDatosBasicosAutores();
         const autoresData = datosResponse;
 
-        // Obtener datos de autores y documentos combinados
         const autores = await obtenerAutores();
         const documentos = await obtenerDocumentos();
 
-        // Procesar datos para cada autor
         const autoresConDatosCompletos = autores.map((author) => {
           const autorId = author["dc:identifier"].split(":")[1];
           const autorData = autoresData.find(
@@ -51,99 +54,156 @@ const AutorSlider = () => {
           };
         });
 
-        // Establecer datos en el estado y contar autores cargados
         setAutoresData(autoresConDatosCompletos);
-        setAutoresLoadedCount(autoresConDatosCompletos.length);
-
-        // Desactivar loading
         setLoading(false);
-        console.log("Se cargaron los 18 autores:", autoresConDatosCompletos);
       } catch (error) {
         console.error("Error al obtener datos:", error);
-        setLoading(false); // Manejar error estableciendo loading como falso
+        setLoading(false);
       }
     };
 
     fetchData();
-  }, []); // Solo se ejecuta una vez al montar el componente
+  }, []);
 
   useEffect(() => {
-    if (!loading && autoresData.length > 0) {
-      // Activar el slider después de 2 minutos si no se ha alcanzado el número deseado
-      const timeout = setTimeout(() => {
-        const interval = setInterval(() => {
-          setCurrentIndex((prevIndex) =>
-            prevIndex === autoresData.length - 1 ? 0 : prevIndex + 1
-          );
-        }, 3000); // Cambia cada 3 segundos
+    if (autoresData.length > 0) {
+      const id = setInterval(() => {
+        handleSlide("left");
+      }, 10000);
 
-        return () => clearInterval(interval);
-      }, 120000); // 2 minutos en milisegundos
-
-      return () => clearTimeout(timeout);
+      return () => clearInterval(id);
     }
-  }, [autoresData, loading]);
+  }, [autoresData]);
 
-  const handleClick = (index) => {
-    setCurrentIndex(index);
+  const handleSlide = (direction) => {
+    setIsAnimating(true);
+    setSlideDirection(direction);
+
+    setTimeout(() => {
+      setCurrentIndex((prevIndex) => {
+        if (direction === "left") {
+          return prevIndex === autoresData.length - 1 ? 0 : prevIndex + 1;
+        } else {
+          return prevIndex === 0 ? autoresData.length - 1 : prevIndex - 1;
+        }
+      });
+      setIsAnimating(false);
+    }, 800);
   };
 
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? autoresData.length - 1 : prevIndex - 1
-    );
+  const handleSwipeLeft = () => {
+    handleSlide("left");
   };
 
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === autoresData.length - 1 ? 0 : prevIndex + 1
-    );
+  const handleSwipeRight = () => {
+    handleSlide("right");
+  };
+
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: handleSwipeLeft,
+    onSwipedRight: handleSwipeRight,
+  });
+
+  if (loading) {
+    return <div>Cargando autores...</div>;
+  }
+
+  const autor = autoresData[currentIndex];
+
+  const getFontSize = (index) => {
+    const baseSize = 1.5; // Tamaño base inicial
+    const decrement = 0.25; // Decremento de tamaño por cada grado académico
+
+    // Limitar el tamaño mínimo después del tercer grado
+    if (index >= 3) {
+      return `${baseSize - 2 * decrement}rem`; // Tamaño fijo a partir del tercer grado
+    }
+
+    return `${baseSize - index * decrement}rem`; // Decremento normal hasta el tercer grado
   };
 
   return (
-    <div className="container">
-      <h1 className="mt-4">Slider de Autores</h1>
-      <div className="slider">
-        {/* Flecha izquierda */}
-        <div className="arrow left-arrow" onClick={handlePrev}>
-          &#60;
-        </div>
-        {/* Renderizar el contenido del slider */}
-        {loading ? (
-          <div>Cargando datos...</div>
-        ) : (
-          autoresData.map((autor, index) => (
-            <div
-              className={`slider-content ${
-                index === currentIndex ? "active-slide" : ""
-              }`}
-              key={autor.autorId}
-              style={{
-                transform: `translateX(${(index - currentIndex) * 100}%)`,
-              }}
-              onClick={() => handleClick(index)}
-            >
-              <img
-                src={autor.rutaImagen}
-                alt={`Imagen de ${autor.nombreCompleto}`}
-              />
-              <h2>{autor.nombreCompleto}</h2>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: autor.gradosAcademicos,
-                }}
-              ></p>
-              <p>Total de Citas: {autor.totalCitas}</p>
-              <p>Total de Documentos: {autor.totalDocumentos}</p>
+    <>
+      <section className="author-banner">
+        <div
+          {...swipeHandlers}
+          className={`slider ${
+            isAnimating
+              ? slideDirection === "left"
+                ? "slide-left"
+                : "slide-right"
+              : ""
+          }`}
+        >
+          <div className="container">
+            <div className="row row-slider">
+              <div className="col-md-4 col-xs-12 autor-image">
+                <img
+                  src={autor.rutaImagen}
+                  alt={`Imagen de ${autor.nombreCompleto}`}
+                  className="img-fluid autor-img"
+                />
+              </div>
+              <div className="col-md-4 col-xs-12 author-details">
+                <div className="datos-autor">
+                  <div className="author-research-output">
+                    <div className="author-name">
+                      <h1>{autor.nombreCompleto}</h1>
+                      {autor.gradosAcademicos
+                        .split("<br>")
+                        .map((grado, index) => (
+                          <h5
+                            key={index}
+                            style={{ fontSize: getFontSize(index) }}
+                          >
+                            {grado}
+                          </h5>
+                        ))}
+                    </div>
+                    <div className="detalles-autor">
+                      <div>
+                        <h1>
+                          N<span>ro </span>
+                          {autor.totalCitas}
+                        </h1>
+                        <p>Citaciones</p>
+                      </div>
+                      <div className="documentos">
+                        <h1>{autor.totalDocumentos}</h1>
+                        <p>Documentos</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          ))
-        )}
-        {/* Flecha derecha */}
-        <div className="arrow right-arrow" onClick={handleNext}>
-          &#62;
+            <div className="row mt-3">
+              <div className="col">
+                {/* Aquí puedes insertar tus gráficos */}
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </section>
+
+      {/* Logos al final de la página */}
+      <footer className="footer">
+        <div className="container">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-12 col-md-3 col-lg-4 uni-logos d-flex flex-column justify-content-center align-items-center">
+              <div className="row universidad">
+                <img src={uch} alt="Logo UCH" className="img-fluid" />
+              </div>
+              <div className="row laboratorios">
+                <img src={intilab} alt="Logo INTILAB" className="img-fluid" />
+                <img src={ehealth} alt="Logo eHealth" className="img-fluid" />
+                <img src={ciics} alt="Logo CIICS" className="img-fluid" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 };
 
