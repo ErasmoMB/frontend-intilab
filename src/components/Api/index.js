af02799cad0754763239311feb06bbcc2d8577af
ErasmoMB <montufarbe@gmail.com>
2024-11-27 17:30:00 -0500

    actualice la altura de AutorSliderv4

--- src/components/Api/index.js
+++ src/components/Api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-/* const baseUrl = "http://127.0.0.1:5000"; */
-const baseUrl = "https://backend-intilab.onrender.com";
+const baseUrl = "http://127.0.0.1:5000";
+/* const baseUrl = "https://backend-intilab.onrender.com"; */
 
 const api = axios.create({
   baseURL: baseUrl,
